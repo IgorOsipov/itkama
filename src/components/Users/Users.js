91cc7acdd873ccff0c892b0e124e424a9cb1f8f1
IgorOsipov <awesomeigor@gmail.com>
2019-05-11 13:56:49 +0300

    17
    Юзеры теперь рект компонент, аякс запрос перенес в дидмаунт

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,47 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react';
 import cls from './Users.module.css';
 import * as axios from 'axios';
 import userImg from '../../assets/img/user.png'
 
 
+class Users extends Component{
 
-let Users = (props) => {
-
-    if(props.users.length === 0){
+    componentDidMount(){
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => { 
-            props.setUsers(response.data.items)
-            
-        })
-    }        
-    return (
-        <div>
-            {props.users.map((user) => {
-                return (
-                    <div key={user.id} className={cls.wrapper}>
-                        <div className={cls.left_wrapper}>
-                            <div className={cls.avatar} style={{backgroundImage: `url(${ user.photos.small ? user.photos.small : userImg})`}}></div>
-                            {
-                                user.followed 
-                                ? <input type="button" value='unfollow' onClick={() => {props.unfollow(user.id)}}/> 
-                                : <input type="button" value='follow' onClick={() => {props.follow(user.id)}}/>
-                            }
-                            
-                        </div>
-                        <div className={cls.right_wrapper}>
-                            <div className={cls.name_status}>
-                                <h2>{user.name}</h2>
-                                <p>{user.status}</p>
+                this.props.setUsers(response.data.items)
+            })
+    }
+  
+    render(){
+        return (
+            <div>
+                {this.props.users.map((user) => {
+                    return (
+                        <div key={user.id} className={cls.wrapper}>
+                            <div className={cls.left_wrapper}>
+                                <div className={cls.avatar} style={{backgroundImage: `url(${ user.photos.small ? user.photos.small : userImg})`}}></div>
+                                {
+                                    user.followed 
+                                    ? <input type="button" value='unfollow' onClick={() => {this.props.unfollow(user.id)}}/> 
+                                    : <input type="button" value='follow' onClick={() => {this.props.follow(user.id)}}/>
+                                }
+                                
                             </div>
-                            <div className={cls.location}>
-                                <h2>{"user.location.city + , "}</h2>
-                                <h2>{"user.location.country"}</h2>
+                            <div className={cls.right_wrapper}>
+                                <div className={cls.name_status}>
+                                    <h2>{user.name}</h2>
+                                    <p>{user.status}</p>
+                                </div>
+                                <div className={cls.location}>
+                                    <h2>{"user.location.city + , "}</h2>
+                                    <h2>{"user.location.country"}</h2>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                )
-            })}
-        </div>
-    )
-} 
+                    )
+                })}
+            </div>
+        )
+    }
+}
 
 export default Users;
--- src/redux/usersReducer.js
+++ src/redux/usersReducer.js
@@ -34,7 +34,7 @@ const usersReducer = (state = initialState, action) => {
         case SETUSERS:
             return {
                 ...state,
-                users: [...state.users, ...action.users]
+                users: [...action.users]
             }
         default:
             return state
